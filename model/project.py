9387a4f51a142146ba7f33c9f39afd6b1be36288
DiastroniX <DiastroniX@gmail.com>
2019-06-23 19:19:34 +0300

    Update tests (added SOAP)

--- model/project.py
+++ model/project.py
@@ -3,8 +3,9 @@ from sys import maxsize
 
 class Project:
 
-    def __init__(self, name=None):
+    def __init__(self, name=None, id = None):
         self.name = name
+        self.id = id
 
     def __repr__(self):
         return "%s" % (self.name)
@@ -15,5 +16,11 @@ class Project:
     def id_or_max(self):
         if self.id:
             return int(self.id)
+        else:
+            return maxsize
+
+    def sorted_by_name(self):
+        if self.name:
+            return str(self.name)
         else:
             return maxsize
--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,11 +1,17 @@
 from model.project import Project
+import string
+import random
 
+
+def random_projectname(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 def test_add_new_project(app):
     app.session.login("administrator", "root")
-    old_projects = app.project.get_project_list()
-    project = Project(name="Moyproject")
+    old_projects = app.soap.project_list("administrator", "root")
+    project = Project(name=random_projectname("proekt_", 12))
     app.project.creation(project)
-    new_projects = app.project.get_project_list()
+    new_projects = app.soap.project_list("administrator", "root")
     old_projects.append(project)
-    old_projects == new_projects
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
